db5ab3f4d9f4fe656d85fd9c741d5a6ee54bfdb5
wecodegood <codegoodwe@gmail.com>
2025-12-12 08:29:39 +0330

    removed the sample html and added option to search.py to get a little faster, i aded wait_until=dom content came :)

--- src/scraptor/divar/search.py
+++ src/scraptor/divar/search.py
@@ -1,4 +1,4 @@
 def search(p, page):
     encoded = p.replace(' ', '%20')
-    page.goto(f"https://divar.ir/s/iran?q={encoded}") 
+    page.goto(f"https://divar.ir/s/iran?q={encoded}", wait_until="domcontentloaded") 
     pass
